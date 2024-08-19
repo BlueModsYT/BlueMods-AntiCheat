97562955b100d53938fbbb208f0669ca1508e4ba
BlueMods <bluemodsyt@gmail.com>
2024-08-19 22:18:03 +0800

    Add files via upload

---
+++ Anti-Cheating/scripts/commands/main.js
@@ -0,0 +1,26 @@
+// All rights reserved @bluemods.lol - discord account. || please report any bugs or glitches in our discord server https://dsc.gg/bluemods
+
+// BlueMods : Moderation
+import "./moderation/ban.js";
+import "./moderation/kick.js";
+import "./moderation/unban.js";
+import "./moderation/freeze.js";
+import "./moderation/unfreeze.js";
+import "./moderation/cmdsf.js";
+import "./moderation/mute.js";
+import "./moderation/unmute.js";
+import "./moderation/lagclear.js";
+// BlueMods : Operator
+import "./operator/own.js";
+import "./operator/op.js";
+import "./operator/deop.js";
+import "./operator/notify.js";
+// BlueMods : General
+import "./general/help.js";
+import "./general/about.js";
+// BlueMods : Gamemodes
+import "./gamemodes/gma.js";
+import "./gamemodes/gmc.js";
+import "./gamemodes/gms.js";
+import "./gamemodes/gmsp.js";
+import "./gamemodes/vanish.js";
