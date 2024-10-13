fa3b39b73c67568367a693167cd5553728225b42
BlueMods <bluemodsyt@gmail.com>
2024-10-13 11:29:25 +0800

    Create main.js

---
+++ AntiCheat-BP/scripts/main.js
@@ -0,0 +1,46 @@
+import "./commands/modules.js";
+// bluemods: systems
+import "./anticheat/nuker/nuker.js";
+import "./anticheat/isAFK.js";
+import "./anticheat/isLored.js";
+import "./anticheat/isOperator.js";
+import "./anticheat/isCrasher.js";
+import "./anticheat/isReached.js";
+import "./anticheat/isNameSpoof.js";
+// bluemods: chat
+import "./chat/playerChat.js";
+import "./chat/projectTile.js";
+import "./chat/playerBanned.js";
+// bluemods : staff
+import "./commands/staff/troll.js";
+import "./commands/staff/ban.js";
+import "./commands/staff/kick.js";
+import "./commands/staff/freeze.js";
+import "./commands/staff/cmdsf.js";
+import "./commands/staff/mute.js";
+import "./commands/staff/lagclear.js";
+import "./commands/staff/ecwipe.js";
+import "./commands/staff/give.js";
+import "./commands/staff/banitem.js";
+import "./commands/staff/welcome.js";
+import "./commands/staff/clearchat.js";
+import "./commands/staff/invsee.js";
+import "./commands/staff/invwipe.js";
+import "./commands/staff/incombat.js";
+import "./commands/staff/pearl.js";
+// bluemods : staff 
+import "./commands/staff/own.js";
+import "./commands/staff/op.js";
+import "./commands/staff/notify.js";
+import "./commands/staff/trusted.js";
+// bluemods : public
+import "./commands/general/tpa.js";
+import "./commands/general/help.js";
+import "./commands/general/about.js";
+import "./commands/general/spawn.js";
+// bluemods : gamemode
+import "./commands/gamemodes/gma.js";
+import "./commands/gamemodes/gmc.js";
+import "./commands/gamemodes/gms.js";
+import "./commands/gamemodes/gmsp.js";
+import "./commands/gamemodes/vanish.js";
