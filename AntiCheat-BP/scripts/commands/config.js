5fcaea0d66b4865c9457f57328923fd354b83999
BlueMods <bluemodsyt@gmail.com>
2024-10-13 14:53:13 +0800

    Created config.js

---
+++ AntiCheat-BP/scripts/commands/config.js
@@ -0,0 +1,50 @@
+export const main = {
+    prefix: "!",
+    adminTag: "admin",
+    trustTag: "trusted",
+    notifyTag: "notify",
+    developer: "§b@bluemods.lol §7| §3https://dsc.gg/bluemods",
+    bmversion: "§gBeta-v5.5.10",
+    mcversion: "§g1.21.30 §7- §g1.21.31",
+    bmdescription: "§3This is a beta mode, some functions may not work with other add-ons. BlueMods AntiCheat for Minecraft Bedrock is a powerful tool designed to protect your server from hackers & cheaters",
+    player: "§7<§eplayer§7>",
+    reason: "§7[§areason§7]",
+    valuedata: "§7<§evalue§7> [§edata§7]",
+    addremove: "§7<§aadd§7/§cremove§7>",
+    enabledisable: "§7<§aenable§7/§cdisable§7>",
+    general: {
+        help: true,
+        tpa: true,
+        spawn: true,
+        about: true
+    },
+    staff: {
+        ban: true,
+        banitem: true,
+        clearchat: true,
+        cmdsf: true,
+        ecwipe: true,
+        freeze: true,
+        give: true,
+        incombat: true,
+        invsee: true,
+        invwipe: true,
+        lagclear: true,
+        mute: true,
+        notify: true,
+        op: true,
+        own: true,
+        pearl: true,
+        troll: true,
+        trusted: true,
+        welcome: true
+    },
+    gamemodes: {
+        gma: true,
+        gmc: true,
+        gms: true,
+        gmsp: true,
+        vanish: true
+    }
+};
+export default main;
