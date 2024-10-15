2cd24953dd8d1ec3b233b06b6100465d010714c9
BlueMods <bluemodsyt@gmail.com>
2024-10-15 16:31:39 +0800

    Updated gamemodes

--- AntiCheat-BP/scripts/commands/gamemodes/gma.js
+++ AntiCheat-BP/scripts/commands/gamemodes/gma.js
@@ -4,6 +4,19 @@ import main from "../config.js";
 
 // All rights reserved @bluemods.lol - discord account. || Please report any bugs or glitches in our Discord server: https://dsc.gg/bluemods
 
+function isCommandEnabled(commandName) {
+    return main.enabledCommands[commandName] !== undefined ? main.enabledCommands[commandName] : true;
+}
+
+const isAuthorized = (player, commandName) => {
+    if (!isCommandEnabled(commandName)) {
+        player.sendMessage(`§7[§b#§7] §cThis command §e${commandName} §cis currently disabled.`);
+        player.runCommandAsync(`playsound random.break @s`);
+        return false;
+    }
+    return true;
+};
+
 Command.register({
     name: "gma",
     description: "",
@@ -11,6 +24,8 @@ Command.register({
     permission: (player) => player.hasTag(main.adminTag),
 }, (data, args) => {
     const player = data.player;
+    if (!isAuthorized(player, "!gma")) return;
+
     const targetName = args[0] || player.name; // Default to the command executor if no target name is provided
     const [targetPlayer] = world.getPlayers({ name: targetName });
 
