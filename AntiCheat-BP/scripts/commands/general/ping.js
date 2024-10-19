b4db6c58eefb7ad2d4b984fbfdb019a60fd8c8c0
BlueMods <bluemodsyt@gmail.com>
2024-10-19 12:54:18 +0800

    Improve ping.js

--- AntiCheat-BP/scripts/commands/general/ping.js
+++ AntiCheat-BP/scripts/commands/general/ping.js
@@ -1,5 +1,5 @@
 import { Command } from "../CommandHandler.js";
-import { system } from "@minecraft/server";
+import { system, world } from "@minecraft/server";
 
 // All rights reserved @bluemods.lol - discord account. || Please report any bugs or glitches in our Discord server: https://dsc.gg/bluemods
 
@@ -7,22 +7,23 @@ Command.register({
     name: "ping",
     description: "",
     aliases: [],
-}, (data) => {
+}, async (data) => {
     const { player } = data;
+    const start = Date.now();
 
-    let ticksPerSecond = 20;
-    let tickInterval = system.currentTick % 20;
+    await player.runCommandAsync(`testfor @s`);
 
-    let worldPing = Math.round(ticksPerSecond - tickInterval) * 50;
-    let worldTPS = Math.min(20, ticksPerSecond);
-
+    const responseTime = Date.now() - start;
+    
     let pingStatus = "§aHigh";
-    if (worldPing > 150) {
+    if (responseTime > 150) {
         pingStatus = "§cLow";
-    } else if (worldPing > 75) {
+    } else if (responseTime > 75) {
         pingStatus = "§gMedium";
     }
 
-    player.sendMessage(`§7[§a#§7] §aPing§7: §e${worldPing}ms §7[${pingStatus}§7] | §aTPS: §e${worldTPS}§7/§e20`);
+    const worldTPS = Math.min(20, 20);
+    player.sendMessage(`§7[§a#§7] §aPing§7: §e${responseTime}ms §7[${pingStatus}§7] | §aTPS: §e${worldTPS}§7/§e20`);
+
     player.runCommandAsync(`playsound random.orb @s`);
 });
