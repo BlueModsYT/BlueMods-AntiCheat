a14f1fcb0f18605017c264c8365bb0c412b7e934
BlueShadow <bluemodsyt@gmail.com>
2024-11-17 05:49:52 +0800

    <a:wumpblush:1307459943913357323>

---
+++ bluemods/scripts/main.js
@@ -0,0 +1,22 @@
+// bluemods: worldSystems
+import "./systems/nuker/nuker.js";
+import "./systems/is32k.js";
+import "./systems/isAFK.js";
+import "./systems/isLored.js";
+import "./systems/isPhase.js";
+import "./systems/isCrasher.js";
+import "./systems/isReached.js";
+import "./systems/isNameSpoof.js";
+import "./systems/items/isIllegal.js";
+// bluemods: chatFilter
+import "./chat/playerChat.js";
+import "./chat/projectTile.js";
+import "./chat/playerBanned.js";
+// bluemods : staffCommands
+import "./commands/general.js";
+import "./commands/gamemodes.js";
+import "./commands/staff-commands.js";
+// bluemods: Developments
+import "./commands/development/cmdtoggle.js";
+import "./commands/development/incombat.js";
+import "./commands/development/nbtload.js";
