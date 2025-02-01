76cb384047236b1b955ba1b815e62639726cf041
BlueShadow <bluemodsyt@gmail.com>
2025-02-02 01:46:34 +0800

    Added isCrasher.js

---
+++ bluemods/scripts/systems/isCrasher.js
@@ -0,0 +1,24 @@
+import { world, system } from "@minecraft/server";
+import main from "../commands/config.js";
+
+// all rights reserve @bluemods.lol - discord account. || please report any bugs or glitches in our discord server https://dsc.gg/bluemods.
+
+const adminTag = main.adminTag
+
+system.runInterval(() => {
+  for (const player of world.getPlayers()) {
+    if (player.hasTag(adminTag)) return;
+    if (
+      Math.abs(player.location.x) > 30000000 ||
+      Math.abs(player.location.y) > 30000000 ||
+      Math.abs(player.location.z) > 30000000
+    ) {
+      player.runCommandAsync(`kick "${player.name}" \n§bBlueMods §7>> §aYou have been kicked out from the server.\n§eReason§7: §cInvalid Item Detected.`);
+      // Notification for Admins
+      world.getPlayers({ tags: ["notify"] }).forEach(admin => {
+            admin.sendMessage(`§7[§d#§7] §e${player.name} §ahas been kicked out for trying to crash the server.`);
+            admin.runCommandAsync(`playsound random.break @s`);
+      });
+    }
+  }
+}, 1);
