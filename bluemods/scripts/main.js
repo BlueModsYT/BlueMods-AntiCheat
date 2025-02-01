ac696bedc5fabe13800668b99293f5ee4fd43422
BlueShadow <bluemodsyt@gmail.com>
2025-02-02 01:01:06 +0800

    Create main.js

---
+++ bluemods/scripts/main.js
@@ -0,0 +1,23 @@
+// bluemods: worldSystems
+import "./systems/8craftersDebugSticksConnectivity.js";
+import "./systems/nuker/nuker.js";
+import "./systems/isAFK.js";
+import "./systems/isLored.js";
+import "./systems/isCrasher.js";
+import "./systems/isNameSpoof.js";
+import "./systems/items/isIllegal.js";
+// bluemods: chatFilter
+import "./chat/playerChat.js";
+import "./chat/projectTile.js";
+import "./chat/playerBanned.js";
+// bluemods : staffCommands
+import "./commands/general.js";
+import "./commands/gamemodes.js";
+import "./commands/staff-commands.js";
+// bluemods: Developments
+import "./commands/development/daily.js";
+import "./commands/development/cmdtoggle.js";
+import "./commands/development/nbtload.js";
+import "./commands/development/spawn.js";
+import "./commands/development/echest.js";
+import "./commands/development/help.js";
