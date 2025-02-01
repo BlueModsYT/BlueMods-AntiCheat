fb8a48e3222e126fee9eb156d16d7970c0e8b0e2
BlueShadow <bluemodsyt@gmail.com>
2025-02-02 01:57:32 +0800

    Added config/moderation.js

---
+++ bluemods/scripts/systems/nuker/config/moderation.js
@@ -0,0 +1,51 @@
+// all rights reserved @bluemods.lol - discord account. || Please report any bugs or glitches in our Discord server: https://dsc.gg/bluemods
+export const BLOCK_CONTAINERS = [
+    "minecraft:lit_furnace",
+    "minecraft:lit_blast_furnace",
+    "minecrart:lit_smoker",
+    "minecraft:brewing_stand",
+    "minecraft:chest",
+    "minecraft:trapped_chest",
+    "minecraft:dispenser",
+    "minecraft:dropper",
+    "minecraft:furnace",
+    "minecraft:blast_furnace",
+    "minecraft:smoker",
+    "minecraft:hopper",
+    "minecraft:barrel",
+    "minecraft:shulker_box",
+    "minecraft:ender_chest",
+    "minecraft:colored_shulker_box",
+    "minecraft:crafter",
+    "minecrart:command_block",
+    "minecrart:repeating_command_block",
+    "minecrart:chain_command_block",
+    "minecrart:structure_block",
+    "minecrart:jigsaw"
+],
+
+VAILD_BLOCK_TAGS = [
+    "snow",
+    "lush_plants_replaceable",
+    "azalea_log_replaceable",
+    "minecraft:crop",
+    "fertilize_area"
+],
+
+IMPOSSIBLE_BREAKS = [
+    "minecraft:invisiblebedrock",
+    "minecraft:water",
+    "minecraft:flowing_water",
+    "minecraft:lava",
+    "minecraft:flowing_lava",
+    "minecraft:bedrock",
+    "minecraft:deny",
+    "minecraft:allow",
+    "minecraft:border_block",
+    "minecraft:command_block",
+    "minecraft:barrier",
+    "minecraft:border_block",
+    "minecraft:structure_block",
+    "minecraft:structure_void",
+    "minecraft:jigsaw"
+];
