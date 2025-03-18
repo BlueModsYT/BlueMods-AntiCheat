2af8476134583a2f8fa5b0b144f6f228e957ae97
BlueMods <bluemodsyt@gmail.com>
2025-03-18 10:35:17 +0800

    Update main.js

--- bluemods/scripts/main.js
+++ bluemods/scripts/main.js
@@ -1,14 +1,16 @@
 // bluemods: worldSystems
 import "./systems/8craftersDebugSticksConnectivity.js";
 import "./systems/nuker/isNuker.js";
-import "./systems/isLored.js";
 import "./systems/isCrasher.js";
 import "./systems/isNameSpoof.js";
 import "./systems/isIllegal.js";
+import "./systems/isEnchantment.js";
 // bluemods: chatFilter
 import "./chat/playerChat.js";
 import "./chat/projectTile.js";
 import "./chat/playerBanned.js";
+import "./chat/playerCompass.js";
+import "./chat/playerEmojis.js";
 // bluemods : staffCommands
 import "./commands/general.js";
 import "./commands/gamemodes.js";
@@ -20,3 +22,7 @@ import "./commands/development/nbtload.js";
 import "./commands/development/spawn.js";
 import "./commands/development/echest.js";
 import "./commands/development/help.js";
+import "./commands/development/back.js";
+import "./commands/development/compass.js";
+
+import "./translations.js";
