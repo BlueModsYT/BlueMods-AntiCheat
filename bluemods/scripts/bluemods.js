11b45ccd9fcb4816b20d25b22bdf8e1a55cf22e1
BlueMods <bluemodsyt@gmail.com>
2025-03-26 12:46:48 +0800

    Update bluemods.js

--- bluemods/scripts/bluemods.js
+++ bluemods/scripts/bluemods.js
@@ -1,26 +1,29 @@
 import { system } from "@minecraft/server";
-const Plugins = ["main.js"]
 
-// all rights reserved @bluemods.lol - discord account. || Please report any bugs or glitches in our Discord server: https://dsc.gg/bluemods
+const Plugins = ["main.js"];
 
-for (const plugin of Plugins) {
+// All rights reserved @bluemods.lol - Discord account. Please report any bugs or glitches in our Discord server: https://dsc.gg/bluemods
 
-    const start = Date.now();
+(async function loadPlugins() {
+    await system.waitTicks(0);
 
-    import(`./${plugin}`)
+    for (const plugin of Plugins) {
+        const start = Date.now();
 
-        .then(() => {
-            console.warn(
-                `§aLoaded Plugin: §e${plugin}§a Successfully, in §e${Date.now() - start} ms§r`
-        );
-    })
-
-    .catch((error) => {
-        console.warn(
-            `Error on Loading Plugin ${plugin}: ` + error + error.stack);
-    });
-}
+        import(`./${plugin}`)
+            .then(() => {
+                console.warn(
+                    `§aLoaded Plugin: §e${plugin}§a Successfully, in §e${Date.now() - start} ms§r`
+                );
+            })
+            .catch((error) => {
+                console.warn(
+                    `Error on Loading Plugin ${plugin}: ` + error + error.stack
+                );
+            });
+    }
+})();
 
 system.beforeEvents.watchdogTerminate.subscribe(data => {
-    data.cancel = true
-}) 
+    data.cancel = true;
+});
