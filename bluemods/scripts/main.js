1154c7ba1f8bb87931dad9f027bf71b62e013390
BlueMods <bluemodsyt@gmail.com>
2025-03-31 02:52:40 +0800

    Update main.js

--- bluemods/scripts/main.js
+++ bluemods/scripts/main.js
@@ -16,13 +16,5 @@ import "./commands/general.js";
 import "./commands/gamemodes.js";
 import "./commands/staff-commands.js";
 // bluemods: Developments
-import "./commands/development/daily.js";
-import "./commands/development/cmdtoggle.js";
-import "./commands/development/nbtload.js";
-import "./commands/development/spawn.js";
-import "./commands/development/echest.js";
-import "./commands/development/help.js";
-import "./commands/development/back.js";
-import "./commands/development/compass.js";
 
 import "./translations.js";
